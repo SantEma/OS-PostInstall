7c1e3352782516095c243192276fbd17c1c48227
Emanuele Santoruvo <e.santoruvo1@studenti.uniba.it>
2025-04-16 20:06:58 +0200

    Top commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Repo per aiutarmi a installare Fedora sul mio amato Thinkpad T16 Gen 2 AMD
+
+Programmi che oramai uso (non inclusi nello script):
+- Termius
+- Parsec
+- Youtube Music (client di terze parti su Github)
---
+++ fedora_things_to_do.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+# "Things To Do!" script for a fresh Fedora Workstation installation
+
+
+
+# Check if the script is run with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with sudo"
+    exit 1
+fi
+
+# Funtion to echo colored text
+color_echo() {
+    local color="$1"
+    local text="$2"
+    case "$color" in
+        "red")     echo -e "\033[0;31m$text\033[0m" ;;
+        "green")   echo -e "\033[0;32m$text\033[0m" ;;
+        "yellow")  echo -e "\033[1;33m$text\033[0m" ;;
+        "blue")    echo -e "\033[0;34m$text\033[0m" ;;
+        *)         echo "$text" ;;
+    esac
+}
+
+# Set variables
+ACTUAL_USER=$SUDO_USER
+ACTUAL_HOME=$(eval echo ~$SUDO_USER)
+LOG_FILE="/var/log/fedora_things_to_do.log"
+INITIAL_DIR=$(pwd)
+
+# Function to generate timestamps
+get_timestamp() {
+    date +"%Y-%m-%d %H:%M:%S"
+}
+
+# Function to log messages
+log_message() {
+    local message="$1"
+    echo "$(get_timestamp) - $message" | tee -a "$LOG_FILE"
+}
+
+# Function to handle errors
+handle_error() {
+    local exit_code=$?
+    local message="$1"
+    if [ $exit_code -ne 0 ]; then
+        color_echo "red" "ERROR: $message"
+        exit $exit_code
+    fi
+}
+
+# Function to prompt for reboot
+prompt_reboot() {
+    sudo -u $ACTUAL_USER bash -c 'read -p "It is time to reboot the machine. Would you like to do it now? (y/n): " choice; [[ $choice == [yY] ]]'
+    if [ $? -eq 0 ]; then
+        color_echo "green" "Rebooting..."
+        reboot
+    else
+        color_echo "red" "Reboot canceled."
+    fi
+}
+
+# Function to backup configuration files
+backup_file() {
+    local file="$1"
+    if [ -f "$file" ]; then
+        cp "$file" "$file.bak"
+        handle_error "Failed to backup $file"
+        color_echo "green" "Backed up $file"
+    fi
+}
+
+echo "";
+echo "╔═════════════════════════════════════════════════════════════════════════════╗";
+echo "║                                                                             ║";
+echo "║   ░█▀▀░█▀▀░█▀▄░█▀█░█▀▄░█▀█░░░█░█░█▀█░█▀▄░█░█░█▀▀░▀█▀░█▀█░▀█▀░▀█▀░█▀█░█▀█░   ║";
+echo "║   ░█▀▀░█▀▀░█░█░█░█░█▀▄░█▀█░░░█▄█░█░█░█▀▄░█▀▄░▀▀█░░█░░█▀█░░█░░░█░░█░█░█░█░   ║";
+echo "║   ░▀░░░▀▀▀░▀▀░░▀▀▀░▀░▀░▀░▀░░░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░░▀░░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀░   ║";
+echo "║   ░░░░░░░░░░░░▀█▀░█░█░▀█▀░█▀█░█▀▀░█▀▀░░░▀█▀░█▀█░░░█▀▄░█▀█░█░░░░░░░░░░░░░░   ║";
+echo "║   ░░░░░░░░░░░░░█░░█▀█░░█░░█░█░█░█░▀▀█░░░░█░░█░█░░░█░█░█░█░▀░░░░░░░░░░░░░░   ║";
+echo "║   ░░░░░░░░░░░░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░░░░▀░░▀▀▀░░░▀▀░░▀▀▀░▀░░░░░░░░░░░░░░   ║";
+echo "║                                                                             ║";
+echo "╚═════════════════════════════════════════════════════════════════════════════╝";
+echo "";
+echo "This script automates \"Things To Do!\" steps after a fresh Fedora Workstation installation"
+echo "ver. 25.03"
+echo ""
+echo "Don't run this script if you didn't build it yourself or don't know what it does."
+echo ""
+read -p "Press Enter to continue or CTRL+C to cancel..."
+
+# System Upgrade
+color_echo "blue" "Performing system upgrade... This may take a while..."
+dnf upgrade -y
+
+
+# System Configuration
+# Set the system hostname to uniquely identify the machine on the network
+color_echo "yellow" "Setting hostname..."
+hostnamectl set-hostname T16G2
+
+# Optimize DNF package manager for faster downloads and efficient updates
+color_echo "yellow" "Configuring DNF Package Manager..."
+backup_file "/etc/dnf/dnf.conf"
+echo "max_parallel_downloads=10" | tee -a /etc/dnf/dnf.conf > /dev/null
+dnf -y install dnf-plugins-core
+
+# Replace Fedora Flatpak Repo with Flathub for better package management and apps stability
+color_echo "yellow" "Replacing Fedora Flatpak Repo with Flathub..."
+dnf install -y flatpak
+flatpak remote-delete fedora --force || true
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak repair
+flatpak update
+
+# Check and apply firmware updates to improve hardware compatibility and performance
+color_echo "yellow" "Checking for firmware updates..."
+fwupdmgr refresh --force
+fwupdmgr get-updates
+fwupdmgr update -y
+
+# Enable RPM Fusion repositories to access additional software packages and codecs
+color_echo "yellow" "Enabling RPM Fusion repositories..."
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf group update core -y
+
+# Install multimedia codecs to enhance multimedia capabilities
+color_echo "yellow" "Installing multimedia codecs..."
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+dnf update @sound-and-video -y
+
+# Install Hardware Accelerated Codecs for AMD GPUs. This improves video playback and encoding performance on systems with AMD graphics.
+color_echo "yellow" "Installing AMD Hardware Accelerated Codecs..."
+dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+
+# Install virtualization tools to enable virtual machines and containerization
+color_echo "yellow" "Installing virtualization tools..."
+dnf install -y @virtualization
+
+
+# App Installation
+# Install essential applications
+color_echo "yellow" "Installing essential applications..."
+dnf install -y htop fastfetch unzip unrar git wget curl
+color_echo "green" "Essential applications installed successfully."
+
+# Install Internet & Communication applications
+color_echo "yellow" "Installing Firefox..."
+flatpak install -y flathub org.mozilla.firefox
+color_echo "green" "Firefox installed successfully."
+color_echo "yellow" "Installing Google Chrome..."
+flatpak install -y flathub com.google.Chrome
+color_echo "green" "Google Chrome installed successfully."
+color_echo "yellow" "Installing Discord..."
+flatpak install -y flathub com.discordapp.Discord
+color_echo "green" "Discord installed successfully."
+
+# Install Office Productivity applications
+color_echo "yellow" "Installing Obsidian..."
+flatpak install -y flathub md.obsidian.Obsidian
+color_echo "green" "Obsidian installed successfully."
+
+# Install Coding and DevOps applications
+color_echo "yellow" "Installing Visual Studio Code..."
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install -y code
+color_echo "green" "Visual Studio Code installed successfully."
+
+# Install Media & Graphics applications
+color_echo "yellow" "Installing VLC..."
+flatpak install -y flathub org.videolan.VLC
+color_echo "green" "VLC installed successfully."
+color_echo "yellow" "Installing OBS Studio..."
+dnf install -y obs-studio
+color_echo "green" "OBS Studio installed successfully."
+
+# Install Remote Networking applications
+color_echo "yellow" "Installing AnyDesk..."
+flatpak install -y flathub com.anydesk.Anydesk
+color_echo "green" "AnyDesk installed successfully."
+
+# Install System Tools applications
+color_echo "yellow" "Installing Gear Lever..."
+flatpak install -y flathub it.mijorus.gearlever
+color_echo "green" "Gear Lever installed successfully."
+
+
+# Customization
+
+
+# Custom user-defined commands
+
+
+# Before finishing, ensure we're in a safe directory
+cd /tmp || cd $ACTUAL_HOME || cd /
+
+# Finish
+echo "";
+echo "╔═════════════════════════════════════════════════════════════════════════╗";
+echo "║                                                                         ║";
+echo "║   ░█░█░█▀▀░█░░░█▀▀░█▀█░█▄█░█▀▀░░░▀█▀░█▀█░░░█▀▀░█▀▀░█▀▄░█▀█░█▀▄░█▀█░█░   ║";
+echo "║   ░█▄█░█▀▀░█░░░█░░░█░█░█░█░█▀▀░░░░█░░█░█░░░█▀▀░█▀▀░█░█░█░█░█▀▄░█▀█░▀░   ║";
+echo "║   ░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░▀░░▀▀▀░░░▀░░░▀▀▀░▀▀░░▀▀▀░▀░▀░▀░▀░▀░   ║";
+echo "║                                                                         ║";
+echo "╚═════════════════════════════════════════════════════════════════════════╝";
+echo "";
+color_echo "green" "All steps completed. Enjoy!"
+
+# Prompt for reboot
+prompt_reboot
